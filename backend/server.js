5b28408710021d8244ae8a1135e15ba243e385ff
Ernst Larsen <aaari94@gmail.com>
2018-12-12 19:23:51 +0100

    lol

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,22 @@ const app = express();
 const port = 5000;
 
 app.get('/', (request, response) => {
-	response.send(`Hello, I'm alive`);
+	response.send(`Hello World.`);
 	logger.info('Responded to GET request on /')
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Arien Shibani", "location": "Bergen, Norway"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2010", "yearTo": "2013", "place": "Haugli Bakeri", "comment": "Bakte masse brød"}, {"yearFrom": "2015", "yearTo": "2018", "place": "Teknik Magasinet", "comment": "Salesman"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "2011", "yearTo": "2013", "place": "Vardafjell VGS", "comment": "Studying"}, {"yearFrom": "2016", "yearTo": "2018", "place": "University of Bergen", "comment": "Bachelor in information sciences"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
